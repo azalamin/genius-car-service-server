99ffec863cd5cde0dbc7836ebfd1342163ce9b99
Al-amin Sheikh <azalamins@gmail.com>
2022-04-26 02:37:54 +0600

    added server deploy steps

---
+++ __server-deploy.js
@@ -0,0 +1,10 @@
+/**
+ * ----------ONe TIME For Computer-----------
+ * 1. Create Heroku account
+ * 2. Verify email
+ * 3. Install heroku cli
+ * 4. Heroku login
+ * 
+ * ----------Each Project One time-----------
+ * 1. heroku create
+*/
